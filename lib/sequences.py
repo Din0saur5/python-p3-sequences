4f1e821d85619f0abb64c52e4df7464a4e7993b8
Daniel Hackworth <dan.hackworth.515@gmail.com>
2023-11-16 13:34:00 -0500

    lab complete

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fib_list = [0]
+    if length == 0:
+        print([])
+    else:
+        if length == 1:
+            print(fib_list)
+        else:
+            fib_list.append(1)
+            for num in range(1,length-1):
+                nxt_num= fib_list[num]+fib_list[num-1]
+                fib_list.append(nxt_num)
+            print(fib_list)
+         
+         
+        
